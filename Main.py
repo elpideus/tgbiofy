29ee3dff3fedc4d88a7d77ab8fbdc73b38d5dd44
elpideus <elpideus@gmail.com>
2021-03-25 18:16:23 +0100

    Photo duplicates Fix

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
 """ This module updates user's Telegram information when his song on Spotify is changed.
 Author: elpideus <elpideus@gmail.com>
 Version: Beta 1.0 """
-from telethon.tl.functions.photos import UploadProfilePhotoRequest
+from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
 from telethon.tl.functions.account import UpdateProfileRequest
 from telethon.tl.functions.users import GetFullUserRequest
-from spotipy.oauth2 import SpotifyOAuth
 from telethon import TelegramClient
 from datetime import datetime
 from termcolor import colored
@@ -15,51 +14,49 @@ import time
 
 
 config = configparser.ConfigParser()
-config.read("config.ini")  # Reads the configuration file
+config.read("config.ini")
 client = TelegramClient(
     'default',
     int(config["!USER!"]["tg_api_id"]),
-    config["!USER!"]["tg_api_hash"])  # Creates a connection to Telegram
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-currently-playing",
-                                               client_id=config["!SPOTIFY!"]["client"],
-                                               client_secret=config["!SPOTIFY!"]["secret"],
-                                               redirect_uri=config["!SPOTIFY!"]["redirect"]
-                                               ))  # Connects to Spotify API
+    config["!USER!"]["tg_api_hash"])
+token = spotipy.util.prompt_for_user_token(config["!SPOTIFY!"]["username"],
+                                           "user-read-currently-playing",
+                                           config["!SPOTIFY!"]["client"],
+                                           config["!SPOTIFY!"]["secret"],
+                                           config["!SPOTIFY!"]["redirect"])
+sp = spotipy.Spotify(token=token)
 
 
 async def main():
     """ This function is the main function of the program. """
-    me = await client.get_me()  # Gets the user after phone number verification
+    me = await client.get_me()
 
     while True:  # loop
-        info = await client(GetFullUserRequest(id=me.username))  # Gets some info about the Telegram user
+        info = await client(GetFullUserRequest(id=me.username))
         if sp.currently_playing(config["!SPOTIFY!"]["market"]) is not None:
-            # If there is a song playing (no ad). If this is not getting checked the program crashes cause of an error.
             song = sp.currently_playing(config["!SPOTIFY!"]["market"])["item"]
-            # Gets some information about the Spotify song
-            print(song)
-            if info.about != ("Listening to " + song["name"][0:57]):  # Checks if the user Telegram Bio is not the same
-                await client(UpdateProfileRequest(
-                    first_name="Listening to " + song["name"][0:57],
-                    last_name="by " + song["artists"][0]["name"],
-                    about="by " + song["artists"][0]["name"]
-                ))  # Updates the Telegram user names and bio
-                print(datetime.now().strftime("%H:%M:%S > ") + "Listening to " + colored(song["name"][0:57], "green") +
-                      " by " + colored(song["artists"][0]["name"], "green"))
-                urllib.request.urlretrieve(song["album"]["images"][0]["url"], "album.jpg")  # Downloads the album image
-                if info.profile_photo != await client.upload_file("album.jpg"):
+            if (song is not None):
+                if info.about != ("Listening to " + song["name"][0:57]):
+                    await client(UpdateProfileRequest(
+                        first_name="Listening to " + song["name"][0:57],
+                        last_name="by " + song["artists"][0]["name"],
+                        about="by " + song["artists"][0]["name"]
+                    ))
+                    print(datetime.now().strftime("%H:%M:%S > ") + "Listening to " + colored(song["name"][0:57], "green") +
+                          " by " + colored(song["artists"][0]["name"], "green"))
+                    urllib.request.urlretrieve(song["album"]["images"][0]["url"], "album.jpg")
+                    await client(DeletePhotosRequest((await client.get_profile_photos(await client.get_me()))))
                     await client(UploadProfilePhotoRequest(await client.upload_file("album.jpg")))
-                # Updates the Telegram user profile photo
-                time.sleep(30)  # Waits for 30 seconds
-            else:
-                time.sleep(3)  # Waits three seconds
-        else:  # If no song is playing
+                    time.sleep(30)
+                else:
+                    time.sleep(3)
+        else:
             await client(UpdateProfileRequest(
                 about="Not listening to music at the moment",
                 first_name=config["!USER!"]["first_name"],
                 last_name=""
-            ))  # Updates bio and names
-            time.sleep(10)  # Then waits for 10 seconds
+            ))
+            time.sleep(10)
 
 
 with client:
