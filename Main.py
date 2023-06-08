1237cca36db88c252f33a61f56b74c633aeb4f55
Stefan Cucoranu <elpideus@gmail.com>
2023-06-09 01:48:50 +0200

    Added timeout

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 """ This module updates user's Telegram information when his song on Spotify is changed.
 Author: elpideus <elpideus@gmail.com>
-Version: 1.2 """
+Version: 1.3 """
 
 from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
 from telethon.tl.functions.account import UpdateProfileRequest
 from telethon import TelegramClient
 from datetime import datetime
+
+from telethon.tl.functions.users import GetFullUserRequest
 from termcolor import colored
 import urllib.request
 import configparser
@@ -35,11 +37,17 @@ async def main():
     bio = config["!SETTINGS!"]["bio"]
     market = config["!SPOTIFY!"]["market"]
     me = await client.get_me()
-
+    currently_playing_song_verify = ""
+    timeout = 0
+    old_first_name = f'{(me.first_name if me.first_name is not None else "")}'
+    old_last_name = f'{(me.last_name if me.last_name is not None else "")}'
+    old_full = await client(GetFullUserRequest(me))
+    old_about = f'{(old_full.full_user.about if old_full.full_user.about is not None else "")}'
     await client.download_profile_photo(me, "pfpic.jpg", download_big=True)
 
     while True:
-        if sp.currently_playing(market) is not None and sp.currently_playing(market)["item"] is not None:
+        if currently_playing_song_verify != sp.currently_playing(market)["item"]["name"]:
+            timeout = 0
 
             if bio == "link" and lite != "True":
                 await client(UpdateProfileRequest(
@@ -69,6 +77,14 @@ async def main():
                 await client(DeletePhotosRequest((await client.get_profile_photos(me))))
                 await client(UploadProfilePhotoRequest(file=(await client.upload_file("album.jpg"))))
 
+            currently_playing_song_verify = sp.currently_playing(market)["item"]["name"]
+
+        if timeout >= 1:
+            await client(UpdateProfileRequest(first_name=old_first_name, last_name=old_last_name, about=old_about))
+            await client(DeletePhotosRequest((await client.get_profile_photos(me))))
+            await client(UploadProfilePhotoRequest(file=(await client.upload_file("pfpic.jpg"))))
+
+        timeout += 10
         time.sleep(30)  # the only fix that works for now
 
 
